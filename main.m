757aa04585cc56a35c36b70c572aa0ee450a1dda
Will Scheirey <williamscheirey@gmail.com>
2026-03-30 23:40:03 -0400

    added plotting for cp and cna for each part

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
-clear; clc; close all;
+clear; clc;
 
 M0 = 340.3; % Mach at sea level [m/s]
 rho = 1.225; % [kg m^-3]
 
-alpha = deg2rad(3);
+alpha = deg2rad(10);
 
 stages = [1, 2];
 
@@ -20,12 +20,14 @@ for n = 1:num_sections
     R_ref = max(R_ref, sections{n}.aftrad);
 end
 
+A_ref = pi * R_ref^2;
+
 S = pi*R_ref^2;
 
-num_v = 100;
+num_v = 200;
 v_all = linspace(0, 800, num_v);
 
-v = 300; % [m/s]
+v = 100; % [m/s]
 M     = v / M0;
 
 data_all = cell(num_v, 1);
@@ -42,6 +44,8 @@ for n = 1:num_v
     cna_all(n) = data.cna_tot;
 end
 
+%% PLOTTING
+
 plot_bending(ork, M, alpha, stages, data_all{n}, filename);
 
 figure(2)
@@ -57,3 +61,42 @@ plot(v_all / M0, cna_all, 'LineWidth', 2)
 title("CN\alpha vs Mach Number")
 xlabel("M")
 ylabel("CN\alpha")
+
+figure(4); clf; figure(5); clf; figure(6); clf
+
+num_sections = numel(data.aero_sections);
+colors = turbo(num_sections);
+lw = 2;
+for k = 1:num_sections
+    cp_data  = zeros(num_v, 1);
+    cna_data = zeros(num_v, 1);
+    for n = 1:num_v
+        cp_data(n)  = data_all{n}.aero_sections{k}.Cp;
+        cna_data(n) = data_all{n}.aero_sections{k}.CNa;
+    end
+
+    name = data_all{n}.aero_sections{k}.name;
+    start = data_all{n}.aero_sections{k}.x;
+    the_length = data_all{n}.aero_sections{k}.length;
+
+    figure(4)
+    plot(v_all / M0, cp_data, 'LineWidth', lw, 'DisplayName', name, 'Color', colors(k, :)); hold on
+    ylabel('Cp');
+    xlabel('Mach Number');
+    title('Cp Absolute vs Mach')
+    legend
+
+    figure(5)
+    plot(v_all / M0, 100 * (cp_data - start) / the_length, 'LineWidth', lw, 'DisplayName', name, 'Color', colors(k, :)); hold on
+    ylabel('Cp (% Component Length)');
+    xlabel('Mach Number');
+    title('Cp from Component Start vs Mach')
+    legend
+
+    figure(6)
+    plot(v_all / M0, cna_data, 'LineWidth', lw, 'DisplayName', name, 'Color', colors(k, :)); hold on
+    ylabel('CN_\alpha')
+    xlabel('Mach Number');
+    title('CN_\alpha vs Mach')
+    legend
+end
